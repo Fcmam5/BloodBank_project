1ab3ce925af2c07e43375c3f6226d94dddaa5066
nourelhouda31 <nourelhouda.mi@gmail.com>
2015-11-13 21:43:51 +0100

    Update Volunteer.java

--- BloodBank_JAVA/src/persons/Volunteer.java
+++ BloodBank_JAVA/src/persons/Volunteer.java
@@ -5,4 +5,44 @@ package persons;
  * Implemented by
  */
 public class Volunteer extends Personne {
-}
+  
+     
+     private int age ;
+     private double poids ;
+     private int physical_cond_scale ;
+
+    public Volunteer(int age, double poids, String name, String firstName, char grouping) {
+        super(name, firstName, grouping);
+        this.age = age;
+        this.poids = poids;
+    }
+     
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+    
+    public double getPoids() {
+        return poids;
+    }
+     public void setPoids(double poids) {
+        this.poids = poids;
+    }
+
+    public int getPhysical_cond_scale() {
+        return physical_cond_scale;
+    }
+
+    public void setPhysical_cond_scale(int physical_cond_scale) {
+        this.physical_cond_scale = physical_cond_scale;
+    }
+     
+   
+     }
+
