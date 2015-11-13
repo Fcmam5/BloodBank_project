b51451b1a43f047614199e3194b44b6c1da60b3e
fcmam5 <dr.fcmam5@gmail.com>
2015-11-13 19:40:47 +0100

    UI's birth

--- BloodBank_JAVA/src/Main.java
+++ BloodBank_JAVA/src/Main.java
@@ -1,9 +1,13 @@
+
 /**
  * Created by fcmam5 on 11/13/15.
  */
+
+
 public class Main {
+    /**
+     * Launch the application.
+     */
     public static void main(String[] args) {
-        System.out.println("Bismi Allah");
-        // bismi allah are u ready :D 
-    }
-}
+
+    }}
