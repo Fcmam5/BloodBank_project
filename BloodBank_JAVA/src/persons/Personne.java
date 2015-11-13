8a71ed421062649b2af9db7f324886948d355dfc
nesrinehanane <nislafe@gmail.com>
2015-11-13 21:07:27 +0100

    Update Personne.java

--- BloodBank_JAVA/src/persons/Personne.java
+++ BloodBank_JAVA/src/persons/Personne.java
@@ -2,7 +2,32 @@ package persons;
 
 /**
  * Created by fcmam5 on 11/13/15.
- * Implemented by
+ * Implemented by nesrinehanane
  */
 abstract class Personne {
+  private String name;
+    private String firstName;
+    private char grouping;
+
+    public void setName(String name){
+        this.name=name;
+
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public void setGrouping(char grouping){
+        this.grouping=grouping;
+    }
+    public char getGrouping(){
+        return this.grouping;
+    }
+
+
 }
