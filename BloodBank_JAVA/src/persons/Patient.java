16144da111c8afcbf4a598393d7b62a48474ab36
leila-kadri <kadrileila04@gmail.com>
2015-11-13 21:36:08 +0100

    Update Patient.java

--- BloodBank_JAVA/src/persons/Patient.java
+++ BloodBank_JAVA/src/persons/Patient.java
@@ -5,4 +5,24 @@ package persons;
  * Implemented by
  */
 public class Patient extends Personne {
+  
+   private int quantite;
+    private int degre_urg;
+    public Patient(int quantite,int degre_urg){
+        super();
+        this.degre_urg=degre_urg;
+        this.quantite=quantite;
+    }
+    public void setQuantité(int quantité){
+        this.quantite=quantité;
+    }
+    public int getQuantite(){
+        return this.quantite;
+    }
+    public void setDegre_urg(int degre_urg){
+        this.degre_urg=degre_urg;
+    }
+    public int Degre_urg(){
+        return this.degre_urg;
+    }
 }
