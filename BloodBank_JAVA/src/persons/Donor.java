81004ff9605d8bd19c50ca65d69970283c462a83
fcmam5 <dr.fcmam5@gmail.com>
2015-11-14 13:31:48 +0100

    Set a key to donors

--- BloodBank_JAVA/src/persons/Donor.java
+++ BloodBank_JAVA/src/persons/Donor.java
@@ -1,5 +1,7 @@
 package persons;
 
+import java.util.Objects;
+
 /**
  * Created by fcmam5 on 11/13/15.
  * Implemented by Mouilah-Sarra
@@ -7,7 +9,8 @@ package persons;
 public class Donor extends Personne {
     private static int donNum = 0;
     private int quantite;
-    boolean motivation;
+    private boolean motivation;
+    private Object donorCard[] = new Object[3]; // [[int DonorKey]|[String email]|[int telNumber]]
 
     public Donor(String name, String firstName, int age, String grouping, String[] adresse, int quantite, boolean motivation) {
         super(name, firstName, age, grouping, adresse);
@@ -40,5 +43,24 @@ public class Donor extends Personne {
     }
 
 
-}     
+    /**
+     *  if He is motivated let's keep his infos in a table :
+     *  [[String DonorKey]|[String email]|[int telNumber]]
+     *  DonorKey is [firstDon+the name & first's first nameLetter]
+     *      Example : "Johny Bob's first donNum was 54 (Was the the 54th donor)"
+     *                 His code will be : JB54
+     */
+
+    public Donor(String name, String firstName, int age, String grouping, String[] adresse_given, int quantite) {
+        super(name, firstName, age, grouping, adresse_given);
+        this.quantite = quantite;
+        this.motivation = true;
+
+        //Filling DonorCard :
+        this.donorCard[0] = name.charAt(0)+firstName.charAt(0)+this.getDonNum(); //Code B-)
+        this.donorCard[1] = name;
+        this.donorCard[2] = firstName;
+    }
+
+}
 
