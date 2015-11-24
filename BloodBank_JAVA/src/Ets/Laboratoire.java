f5d0a356b4d58be13eaa0d5fe1f5c97de23a3b0e
fcmam5 <dr.fcmam5@gmail.com>
2015-11-24 22:50:27 +0100

    Set classes to fill

--- BloodBank_JAVA/src/Ets/Laboratoire.java
+++ BloodBank_JAVA/src/Ets/Laboratoire.java
@@ -2,6 +2,7 @@ package Ets;
 
 /**
  * Created by fcmam5 on 11/13/15.
+ * Implemented by Sarra
  */
 public class Laboratoire {
 
---
+++ BloodBank_JAVA/src/Ets/ServiceDon.java
@@ -0,0 +1,8 @@
+package Ets;
+
+/**
+ * Created by fcmam5 on 11/24/15.
+ * Implemented by Doudi & Leila
+ */
+public class ServiceDon {
+}
