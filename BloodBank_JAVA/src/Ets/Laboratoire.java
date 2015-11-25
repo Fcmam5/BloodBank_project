bef3f68708b33994f55ae5869f8e7555dcfcdc8f
Mouilah-Sarra <mouilahsarra@gmail.com>
2015-11-25 16:07:59 +0100

    Update Laboratoire.java

--- BloodBank_JAVA/src/Ets/Laboratoire.java
+++ BloodBank_JAVA/src/Ets/Laboratoire.java
@@ -5,7 +5,20 @@ package Ets;
  * Implemented by Sarra
  */
 public class Laboratoire {
-
+ public boolean analyse (boolean RepMedcin){
+        if (RepMedcin==true )
+            return true;
+        else 
+            return false;
+    }
+    public void informer (){
+    
+        /*   Base de Donnée */
+    } 
+    public void envoyer(){
+      /* réseux */
+        
+    }
     //Analyser(RepMedcin:Boolean);
     //Informer() > Mail, Telephone
 
