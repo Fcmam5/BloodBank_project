58984a844b9dac3d40486e29b70771fedd5ad186
Mouilah-Sarra <mouilahsarra@gmail.com>
2015-12-09 16:34:24 +0100

    Update Medecin.java

--- BloodBank_JAVA/src/persons/Medecin.java
+++ BloodBank_JAVA/src/persons/Medecin.java
@@ -4,7 +4,8 @@ import java.util.Date;
  ** Created  on 12/05/15.
  * Implemented by nesrinehanane
  * */
- public class Medecin extends Personne{
+ 
+ 
     
    public class Medecin extends Personne{
     
