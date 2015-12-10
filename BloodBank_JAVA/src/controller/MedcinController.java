ac9f806113f5fd6a1235d1ce60a330f7718e4852
fcmam5 <dr.fcmam5@gmail.com>
2015-12-11 00:34:50 +0100

    Modify MedcinController

---
+++ BloodBank_JAVA/src/controller/MedcinController.java
@@ -0,0 +1,20 @@
+public class MedcinController {
+    private int age ;
+    private boolean derdon;
+
+    public void ajouterDoneur(/*les info de dialog*/) {
+        // base de donnée : ajouter donor
+}
+
+    public boolean verifierAge(){
+        return ((age <18)&&(age>=65))
+            return false ;
+          }
+
+
+    public boolean verPoids(int poids){
+        return poids>=50;
+    }
+
+    //Need : Date du dernier don
+}
