3f1df6abeb6cc191b24a9bf5e61a71cc0db99368
nesrinehanane <nislafe@gmail.com>
2015-12-11 16:02:24 +0100

    Update Donor.java
    il y a beaucoup trop de contructeur !!! et pourquoi les id il est deja declarer dans la classe personne

--- BloodBank_JAVA/src/persons/Donor.java
+++ BloodBank_JAVA/src/persons/Donor.java
@@ -7,58 +7,45 @@ import java.util.Objects;
  * Implemented by Mouilah-Sarra
  */
 public class Donor extends Personne {
-    private static int donNum = 0;
+    
     private int quantite;
-    private boolean motivation;
-   /* private Object donorCard[] = new Object[3];// [[int DonorKey]|[String email]|[int telNumber]] //base de donnée ! pas besoin de ce tableau */
-  private  String mail;
-  private int numTel;
+    private byte motivation;
+   
 
-    public Donor(String name, String firstName, int age, String grouping, String[] adresse, int quantite, boolean motivation,String mail, int numTel) {
-        super(name, firstName, age, grouping, adresse);
-        this.donNum++;
+    public Donor(int id, String name, String firstName, int age, String grouping, String[] adresse, int quantite, boolean motivation,String mail, int numTel) {
+        super( 
+            int id,
+            String name, 
+            String firstName, 
+            int age, 
+            String grouping , 
+            Adresse adresse,
+            String mail, 
+            int phoneNumber,
+            double poids);
+      
         this.quantite = quantite;
         this.motivation = motivation;
-         this.mail=mail;
-        this.numTel=numTel;
+         
     }
 
 
-    public void setGroupage (String gropage){
-        setGrouping(gropage);
-        
-}
-    public String getGroupage(){
-        return getGrouping();
-}
-
-    public int getDonNum( ){
-        return this.donNum;
-       
-    }
-
-    // HOw much he gives ?
-    public void setQuantite (int quantite){
-    this.quantite=quantite;
-}
-
+   
     public int getQuantite (){
         return this.quantite;
     }
-      public void setMail(String mail){
-         this.mail=mail;
-         
+    public void setQuantite(int quantite){
+        this.quantite= quantite;
     }
-    public String getMail(){
-         return mail;
+     
+     public byte getMotivation(){
+         return this.motivisation;
+     }
+    
+    public void setMotivation(byte motivation){
+        this.motivation=motivation;
     }
-    public void setNumTel(int numTel){
-         this.numTel=numTel;
-    }
-    public int getNumTel(){
-         return numTel;
-    }
-
+    
 
     /**
      *  if He is motivated let's keep his infos in a table :
@@ -68,7 +55,7 @@ public class Donor extends Personne {
      *                 His code will be : JB54
      */
 
-    public Donor(String name, String firstName, int age, String grouping, String[] adresse_given, int quantite) {
+    /*public Donor(String name, String firstName, int age, String grouping, String[] adresse_given, int quantite) {
         super(name, firstName, age, grouping, adresse_given);
         this.quantite = quantite;
         this.motivation = true;
@@ -77,7 +64,7 @@ public class Donor extends Personne {
         this.donorCard[0] = name.charAt(0)+firstName.charAt(0)+this.getDonNum(); //Code B-)
         this.donorCard[1] = name;
         this.donorCard[2] = firstName;
-    }
+    }*/ //pourquoi tous ces contructeurs
 
 }
 
