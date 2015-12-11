e1a0c3c745581fef7b5e26333a8fa06bfdaae0b5
nesrinehanane <nislafe@gmail.com>
2015-12-11 22:33:42 +0100

    Update Donor.java

--- BloodBank_JAVA/src/persons/Donor.java
+++ BloodBank_JAVA/src/persons/Donor.java
@@ -7,35 +7,31 @@ import java.util.Objects;
  * Implemented by Mouilah-Sarra
  */
 public class Donor extends Personne {
-    private static int donNum = 0;
+   
     private int quantite;
-    private boolean motivation;
+    private byte motivation;
    /* private Object donorCard[] = new Object[3];// [[int DonorKey]|[String email]|[int telNumber]] //base de donnée ! pas besoin de ce tableau */
   private  String mail;
   private int numTel;
 
-    public Donor(String name, String firstName, int age, String grouping, String[] adresse, int quantite, boolean motivation,String mail, int numTel) {
-        super(name, firstName, age, grouping, adresse);
-        this.donNum++;
+    public Donor(int id, String name, String firstName, int age, String grouping, String[] adresse, int quantite, boolean motivation,String mail, int numTel) {
+        super(id,name, firstName, age, grouping, adresse);
+     
         this.quantite = quantite;
         this.motivation = motivation;
-         this.mail=mail;
-        this.numTel=numTel;
+         
     }
 
 
     public void setGroupage (String gropage){
-        setGrouping(gropage);
+        this.groupage=groupage;
         
 }
     public String getGroupage(){
         return getGrouping();
 }
 
-    public int getDonNum( ){
-        return this.donNum;
-       
-    }
+   
 
     // HOw much he gives ?
     public void setQuantite (int quantite){
@@ -45,18 +41,12 @@ public class Donor extends Personne {
     public int getQuantite (){
         return this.quantite;
     }
-      public void setMail(String mail){
-         this.mail=mail;
-         
+    
+    public void setMotivation(byte motivation){
+        this.motivation=motivation;
     }
-    public String getMail(){
-         return mail;
-    }
-    public void setNumTel(int numTel){
-         this.numTel=numTel;
-    }
-    public int getNumTel(){
-         return numTel;
+    public byte getMotivation(){
+        return this.motivation ;
     }
 
 
@@ -68,7 +58,7 @@ public class Donor extends Personne {
      *                 His code will be : JB54
      */
 
-    public Donor(String name, String firstName, int age, String grouping, String[] adresse_given, int quantite) {
+    /*public Donor(String name, String firstName, int age, String grouping, String[] adresse_given, int quantite) {
         super(name, firstName, age, grouping, adresse_given);
         this.quantite = quantite;
         this.motivation = true;
@@ -77,7 +67,7 @@ public class Donor extends Personne {
         this.donorCard[0] = name.charAt(0)+firstName.charAt(0)+this.getDonNum(); //Code B-)
         this.donorCard[1] = name;
         this.donorCard[2] = firstName;
-    }
+    }*/
 
 }
 
